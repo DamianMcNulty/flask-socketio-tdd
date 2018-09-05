3dcb917b1a362eaf95789336183cada4971e84d4
Juan Batty <jmbl1685@hotmail.com>
2018-09-05 12:13:48 -0500

    Update test.py

--- test.py
+++ test.py
@@ -3,11 +3,9 @@ import json
 
 from server import app
 
-
 class BasicTestCase(unittest.TestCase):
 
     def test_index(self):
-        """initial test. ensure flask was set up correctly"""
         tester = app.test_client(self)
         response = tester.get('/', content_type='html/text')
         self.assertEqual(response.status_code, 200)
